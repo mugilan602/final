eee00f7d9745516dbc9b03f6c06855d48dc9b572
mugilan602 <mugilan7778@gmail.com>
2023-12-05 08:40:55 +0530

    created pool

--- index.js
+++ index.js
@@ -3,68 +3,71 @@ import express from "express";
 import { dirname } from "path";
 import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
-import { log } from "console";
 import dotenv from 'dotenv';
 dotenv.config();
-
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
-const connection = mysql.createConnection({
-  host: process.env.host, 
+
+// Create a connection pool
+const pool = mysql.createPool({
+  connectionLimit: 10, // Adjust as needed
+  host: process.env.host,
   user: process.env.user,
   password: process.env.password,
   database: process.env.database,
+  connectTimeout: 20000, // Adjust as needed (in milliseconds)
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    return;
-  }
-  console.log('Connected to MySQL');
-});
-
-
-
-app.get("/", (req, res) => {
-  connection.query('SELECT roll_number, name, status FROM students', (err, results) => {
+// Middleware to acquire a connection from the pool for each request
+app.use((req, res, next) => {
+  pool.getConnection((err, connection) => {
     if (err) {
-      console.error('Error executing query:', err);
-      return;
+      console.error('Error getting MySQL connection:', err);
+      return res.status(500).send('Internal Server Error');
     }
-  
-    res.render("index.ejs", { results });
+
+    // Attach the connection to the request for use in route handlers
+    req.mysqlConnection = connection;
+
+    // Pass control to the next middleware or route handler
+    next();
   });
 });
 
-
+// Handle routes
+app.get("/", (req, res) => {
+  req.mysqlConnection.query('SELECT roll_number, name, status FROM students', (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      return res.status(500).send('Internal Server Error');
+    }
+    res.render("index.ejs", { results });
+  });
+});
 
 app.post("/submit", (req, res) => {
   const statusUpdates = req.body;
   Object.entries(statusUpdates).forEach(([name, status]) => {
     const query = `UPDATE students SET status = ? WHERE name = ?`;
-    connection.query(query, [status, name], (err, results) => {
+    req.mysqlConnection.query(query, [status, name], (err, results) => {
       if (err) {
         console.error('Error updating database:', err);
+        return res.status(500).send('Internal Server Error');
       }
     });
   });
   res.redirect("/");
 });
 
-  app.listen(process.env.port || 9002, () => {
-    console.log(`Listening on port 3000`);
-  });
-  
+app.use((req, res, next) => {
+  if (req.mysqlConnection) {
+    req.mysqlConnection.release();
+  }
+  next();
+});
 
-
-
-
-
-
-
-
-
-
+app.listen(process.env.port || 9002, () => {
+  console.log(`Listening on port 3000`);
+});
